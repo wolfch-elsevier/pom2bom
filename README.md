52a5f961586ee2ec4c20073f7f2c99a52efc0987
Chris Wolf <c.wolf@elsevier.com>
2022-11-29 12:31:13 -0500

    Add readme, requirements format code.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Convert multimodule Maven project's parent POM to B.O.M.
+
+## Setup
+Use Python-3.9 or higher.
+
+```
+python -m venv venv
+python -m pip install --upgrade pip
+pip install -r dev-requirements.txt
+```
+Edit `pom2bom.py` and change `BASEDIR` near bottom to point to top-level of project directory
--- pom2bom.py
+++ pom2bom.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
+"""
+Scans all the child POMs and extracts dependency versions, removing the "<version/>" from
+the child POMs and putting into the parent "<dependencyManagement/>", i.e. the B.O.M.
+
+If the same dependency has different versions, then "packaging.version.parse_version" is used
+to compare and select highest version.
+
+The new parent and child POMs are written as "pom_new.xml".
+"""
 import logging
 import os
 import re
-import sys
-from collections import OrderedDict
-from packaging.version import parse as parse_version
-from xml.dom import minidom
-from xml.dom import Node
 import xml.etree.cElementTree as ET
+from collections import OrderedDict
+from xml.dom import Node, minidom
 from xml.etree.ElementTree import ParseError
+
+from packaging.version import parse as parse_version
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,26 +36,12 @@ class POMScanner:
     def __init__(self, pom_path) -> None:
         tree = ET.parse(pom_path)
         self.project = tree.getroot()
-        #self.versionprop = {}
+        # self.versionprop = {}
         self.dependency_groups = {}
         self.non_version_props = {}
         self.version_props = {}
         self.scan_for_dependencies()
-    
-    """
-    @property
-    def dependencies(self):
-        return self.dependency_groups
 
-    @property
-    def non_version_props(self):
-        return self.non_version_props
-
-    @property
-    def version_props(self):
-        return self.version_props
-    """
-    
     def scan_for_version_properties(self):
         properties = self.project.find("mvn:properties", ns)
         if not properties:
@@ -60,10 +54,9 @@ class POMScanner:
                 self.non_version_props[local_tag] = property.text.strip()
 
     def scan_for_dependencies(self):
+        """This routine can be called separately just for getting a dependency list from a POM"""
         self.scan_for_version_properties()
-        for dependency in self.project.iter(
-            "{" + MVN_NS + "}dependency"
-        ):
+        for dependency in self.project.iter("{" + MVN_NS + "}dependency"):
             record = {}
             for elem in dependency:
                 field_name = localname(elem.tag)
@@ -80,18 +73,28 @@ class POMScanner:
             )
 
     def render_version(self, version_str):
-        version_mo = self.INTERPOLATE_PAT.match(version_str)
-        version_str_out = self.version_props[version_mo[1]] if version_mo else version_str
-        return version_str_out
+        try:
+            version_mo = self.INTERPOLATE_PAT.match(version_str)
+            version_str_out = (
+                self.version_props[version_mo[1]] if version_mo else version_str
+            )
+            return version_str_out
+        except KeyError:
+            return version_str
 
 
 def localname(tag_name):
+    """chops off the bracketed prefix namespace URI"""
     if tag_name.find("}") == -1:  # no namespace
         return tag_name
     return tag_name[tag_name.find("}") + 1 :]
 
 
 def strip_pom_file(input_pom_path, output_pom_path, properties_to_strip):
+    """Will remove "<version/>" and "<dependencyManagement/>"
+    from child POMs as well as "<property/>" that should only be
+    defined in the parent, e.g. "<project.build.sourceEncoding/>"
+    """
     tree = minidom.parse(input_pom_path)
     elem = tree.documentElement
 
@@ -104,7 +107,7 @@ def strip_pom_file(input_pom_path, output_pom_path, properties_to_strip):
             node.parentNode.removeChild(node)
             node.unlink()
             return
-        
+
         if (
             node.nodeType == Node.ELEMENT_NODE
             and node.nodeName == "dependencyManagement"
@@ -113,7 +116,7 @@ def strip_pom_file(input_pom_path, output_pom_path, properties_to_strip):
             node.parentNode.removeChild(node)
             node.unlink()
             return
-        
+
         for pn, pv in properties_to_strip.items():
             if (
                 node.nodeType == Node.ELEMENT_NODE
@@ -122,7 +125,7 @@ def strip_pom_file(input_pom_path, output_pom_path, properties_to_strip):
             ):
                 node.parentNode.removeChild(node)
                 node.unlink()
-                
+
         for cn in node.childNodes:
             walk(cn)
 
@@ -161,6 +164,7 @@ def update_dependencies(current_dependencies, new_dependencies, module_name):
                 )
                 current_dependencies[group_id][artifact] = artifact_version
                 continue
+            # replace if higher version of same dependency found...
             if parse_version(artifact_version) > parse_version(
                 current_dependencies[group_id][artifact]
             ):
@@ -174,17 +178,19 @@ def update_dependencies(current_dependencies, new_dependencies, module_name):
                 )
                 current_dependencies[group_id][artifact] = artifact_version
 
+
 def insert_bom_into_parent_pom(base_dir, parent_pom_doc, properties, dependencies):
+    """Adds "<dependencyManagement/>" section to parent POM"""
     root = parent_pom_doc.getroot()
     properties_element = root.find(".//mvn:properties", ns)
     for key in sorted(list(properties)):
         ET.SubElement(properties_element, key).text = properties[key]
-         
+
     artifacts = OrderedDict()
     for group_id in dependencies:
         for artifact in dependencies[group_id]:
             artifacts[artifact] = ""
-        
+
     child_count = len(list(root))
     dm_element = ET.Element("dependencyManagement")
     dep_element = ET.SubElement(dm_element, "dependencies")
@@ -193,47 +199,55 @@ def insert_bom_into_parent_pom(base_dir, parent_pom_doc, properties, dependencie
             dependency = ET.SubElement(dep_element, "dependency")
             ET.SubElement(dependency, "groupId").text = group_id
             ET.SubElement(dependency, "artifactId").text = artifact
-            ET.SubElement(dependency, "version").text = "${" + artifact + ".version}" 
+            ET.SubElement(dependency, "version").text = "${" + artifact + ".version}"
             artifacts[artifact] = dependencies[group_id][artifact]
-            
+
     for artifact in artifacts:
-        ET.SubElement(properties_element, artifact + ".version").text = artifacts[artifact]
-                  
+        ET.SubElement(properties_element, artifact + ".version").text = artifacts[
+            artifact
+        ]
+
     root.insert(child_count, dm_element)
-    parent_pom_doc.write(os.path.join(base_dir, "pom_new.xml"), encoding="UTF-8", xml_declaration=False)
+    parent_pom_doc.write(
+        os.path.join(base_dir, "pom_new.xml"), encoding="UTF-8", xml_declaration=False
+    )
 
 
 def scan_and_create_bom(base_dir):
+    """main entry point"""
     properties = {}
     dependencies = {}
 
-    ET.register_namespace("", MVN_NS) # set default namespace
+    ET.register_namespace("", MVN_NS)  # set default namespace
     parent_pom_doc = ET.parse(os.path.join(base_dir, "pom.xml"))
     root = parent_pom_doc.getroot()
-   
+
     mvn_modules = root.findall(".//mvn:module", ns)
     for mvn_module in mvn_modules:
         mvn_module_pom = os.path.join(base_dir, mvn_module.text, "pom.xml")
         if os.path.exists(mvn_module_pom):
             new_mvn_module_pom = os.path.join(base_dir, mvn_module.text, "pom_new.xml")
             pomscanner = POMScanner(mvn_module_pom)
-            update_dependencies(dependencies, pomscanner.dependency_groups, mvn_module.text)
+            update_dependencies(
+                dependencies, pomscanner.dependency_groups, mvn_module.text
+            )
             properties.update(pomscanner.version_props)
             properties_to_strip = {}
             properties_to_strip.update(pomscanner.non_version_props)
             properties_to_strip.update(pomscanner.version_props)
             strip_pom_file(mvn_module_pom, new_mvn_module_pom, properties_to_strip)
-       
+
     insert_bom_into_parent_pom(base_dir, parent_pom_doc, properties, dependencies)
 
 
 if __name__ == "__main__":
-    BASEDIR = "/Users/ma-wolf2/src3/search-sparks-integration"
+    BASEDIR = "/Users/ma-wolf2/src6/Search"
 
     scan_and_create_bom(BASEDIR)
+
     """
-    pomscanner = POMScanner("pylib/emclib/src/emclib/pom.xml")
+    # Example read-only way to scan individual pom.xml for dependency list...
+    pomscanner = POMScanner("pom.xml")
     for groupId, artifacts in pomscanner.dependencies.items():
         print(f"{groupId}: {str(artifacts)}")
     """
-    z = 0
